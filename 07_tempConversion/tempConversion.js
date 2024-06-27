0d5ecf87c95d50b55b8053b103e558a1319c8b0d
melfvius <16796516+ivansimwx@users.noreply.github.com>
2024-06-27 22:44:26 +0800

    Pass all test for exercise 7 if javascript tests

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,11 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(fahrenheitDeg) {
+  let celsiusFinal = Number(((fahrenheitDeg -32) * 5/9).toFixed(1));
+  return celsiusFinal
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(celsiusDeg) {
+  let fahrenheitFinal = Number((celsiusDeg * 9/5 +32).toFixed(1));
+  return fahrenheitFinal;
 };
 
 // Do not edit below this line
