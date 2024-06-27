d60011d6c5dd0536dd11cebc2526cdd082c3e1cd
melfvius <16796516+ivansimwx@users.noreply.github.com>
2024-06-27 22:09:57 +0800

    Pass all test for javascript exercise 5

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,28 @@
-const sumAll = function() {
+const sumAll = function(num1, num2) {
+    let finalNum = 0;
+    let startNum = 0;
+    let endNum = 0;
 
+    //check numbers to sequence loop
+    if (num1 <0 || num2<0 || typeof(num1)!="number" || typeof(num2)!="number" ){
+        return "ERROR";
+    }
+    else if (num1<num2){
+        startNum = num1;
+        endNum = num2;
+    }
+    else {
+        startNum = num2;
+        endNum = num1;
+    }
+
+    //add numbers within range
+    for (let i=startNum; i<=endNum; i++){
+        finalNum += i;
+    }
+
+    return finalNum;
+    
 };
 
 // Do not edit below this line
