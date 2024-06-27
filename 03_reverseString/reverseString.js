9cd9cc3e01967cec3281185b4885235a217fabc6
melfvius <16796516+ivansimwx@users.noreply.github.com>
2024-06-27 17:39:33 +0800

    Pass test for javascript exercise 3

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,21 @@
-const reverseString = function() {
+const reverseString = function(word) {
+    let reverseWordArray = [];
+    let reverseCounter = 0;
+    let reverseWord = "";
 
+    if (word.length<1) {
+        return word;
+    }
+
+    for (let i=word.length-1; i>0; i-- ) {
+        reverseWordArray[reverseCounter] = word[i];
+        reverseCounter++;
+    }
+
+    for (let i=0; i<reverseWordArray.length; i++ ) {
+        reverseWord +=reverseWordArray[i];
+    }
+    return reverseWord;
 };
 
 // Do not edit below this line
