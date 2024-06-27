cf14c8348753c2a5539cee88a8844537017b618f
melfvius <16796516+ivansimwx@users.noreply.github.com>
2024-06-27 17:06:05 +0800

    Complete test 1 of javascript exercise 2

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,10 @@
-const repeatString = function() {
+const repeatString = function(word, repeat) {
+    let newWord = "";
 
+    for (let i=0; i<repeat; i++) {
+        newWord += word;
+    }
+    return newWord;
 };
 
 // Do not edit below this line
