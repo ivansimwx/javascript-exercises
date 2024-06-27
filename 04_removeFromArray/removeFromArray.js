f993c63107b164a6ca14010971837daf571d2b7d
melfvius <16796516+ivansimwx@users.noreply.github.com>
2024-06-27 18:15:55 +0800

    Pass test 1 and 2 of javascript exercise 4

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,11 +1,26 @@
-const removeFromArray = function( arrayGiven, toRemove) {
+const removeFromArray = function( arrayGiven, ...toRemove) {
     let newArray = [];
+    let removeCheck = "";
 
     // add items to newArray if they are not the same as the item to remove
     for (let i=0; i<arrayGiven.length; i++) {
-        if (arrayGiven[i] != toRemove){
+        console.log("loop cycle is " + i)
+        removeCheck = "false";
+
+        for (let j=0; j<toRemove.length; j++) {
+            console.log("Given is " + arrayGiven[i] + ". Remove item is " + toRemove[j]);
+            if (arrayGiven[i] == toRemove[j]){
+                removeCheck = "true";
+            }
+        }
+
+        console.log("removeCheck value is " + removeCheck);
+
+        if (removeCheck == "false") {
             newArray.push(arrayGiven[i]);
         }
+
+        console.log("new array is " + newArray)
     }
 
     return newArray;
