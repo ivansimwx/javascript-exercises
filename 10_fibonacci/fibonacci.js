d4abb442592cf96380d0fd0abbb9e2f170695d11
melfvius <16796516+ivansimwx@users.noreply.github.com>
2024-06-30 23:48:01 +0800

    Complete javascript exercise 10

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,6 +1,32 @@
-const fibonacci = function() {
+const fibonacci = function(input) {
+let position1Before = 1 ; 
+let position2Before = 1 ; 
+const position = parseInt(input);
+let number = 0;
 
-};
+if (position == 0) {
+    console.log("oops1");
+    return 0;
+}
+else if (position < 0) {
+    console.log("oops2");
+    return "OOPS";
+}
+else if (position<3) { //for 1st and 2nd position, return 1
+    console.log("oops3");
+    return 1;
+}
+else { 
+    for (let i=3; i<=position; i++){
+        number = position2Before + position1Before; 
+
+        //update values of position2Before and position 1before
+        position2Before = position1Before;
+        position1Before = number;
+    }
+}
+return number;
+}
 
 // Do not edit below this line
 module.exports = fibonacci;
