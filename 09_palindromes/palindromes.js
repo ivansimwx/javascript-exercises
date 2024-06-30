fde74c01cf1aa481ead87794d1668825226a4fec
melfvius <16796516+ivansimwx@users.noreply.github.com>
2024-06-30 23:06:13 +0800

    Complete javascript exercise 9

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,21 @@
-const palindromes = function () {
+const palindromes = function (phrase) {
+    let phraseTemp = phrase.replace(/[,.!\s]/ig, "").toLowerCase();
+    let arrayTemp = [];
 
+
+    console.log(phraseTemp);
+
+    for (let i=0; i<phraseTemp.length; i++){
+        arrayTemp[i]=phraseTemp.charAt(phraseTemp.length-i-1);
+    }
+     console.log(arrayTemp);
+
+    let phraseFlip = arrayTemp.join("");
+
+    console.log(phraseFlip);
+
+    return (phraseFlip === phraseTemp) ? true : false;
+    console.log(phraseFlip);
 };
 
 // Do not edit below this line
