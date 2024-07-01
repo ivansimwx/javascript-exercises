80cb0af3e0dcfb4b01c0aca826921d9e012b7a3d
melfvius <16796516+ivansimwx@users.noreply.github.com>
2024-07-01 19:58:48 +0800

    Complete javascript exercise 12

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,6 +1,22 @@
-const findTheOldest = function() {
+function ageCal (personObj) {
+    if (!personObj.yearOfDeath){
+        const deathYear = new Date().getFullYear();
+    }
+    else {
+        deathYear = personObj.yearOfDeath;
+    }
+   return deathYear - personObj.yearOfBirth;
+}
 
-};
+const findTheOldest = function(personArr) {
+   const sorted = personArr.toSorted(function(a,b) {
+    return ageCal(b)-ageCal(a);
+    })
+    console.log(sorted[0]);
+    console.log(sorted[0].name);
+    return(sorted[0]);
+    }
+
 
 // Do not edit below this line
 module.exports = findTheOldest;
